629f35116a3d80a5f6f39d757e47d948d34dcb3c
Islam Kamal <islamkamalx1>
2023-08-27 17:35:39 +0300

    Contacts&Team&Invoices usin DataGridView from MUI

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,17 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Box
+      m="20px"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
